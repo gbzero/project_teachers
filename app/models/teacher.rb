a2d6100eb769ef52f1a0dbfc58f8ab8563f4c7aa
gbzero <gabyzoto@gmail.com>
2013-10-17 21:03:48 -0600

    tema sin fondo xD

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,4 +1,7 @@
 class Teacher < ActiveRecord::Base
   has_and_belongs_to_many :courses
   has_many :comments
+
+  validates :name, format: { with: /\A[a-zA-Z|á|é|í|ó|ú|ü]+\z/, message:'Nombre no valido'}
+
 end
