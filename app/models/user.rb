8ac0d841c930b4666d231cf46f99da15cf9f4adb
gbzero <gabyzoto@gmail.com>
2013-11-25 22:06:08 -0700

    modelo usuario

--- app/models/user.rb
+++ app/models/user.rb
@@ -149,7 +149,7 @@ class User < ActiveRecord::Base
     if self.rol_id == self.id_admin
       return Comment.all.each
     else
-      return Comment.all.where('user_id = ?', self.id).each
+      return Comment.where('user_id = ?', self.id).each
     end
   end
 
